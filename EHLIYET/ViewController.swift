eaf6503ad1a1712e8c1918a000ea8bbea7dc1cc7
yilmazer34 <dryilmazer34@gmail.com>
2019-05-07 11:27:09 +0300

    Version 0.0
    Uygulamanın en saf hali

---
+++ EHLIYET/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EHLIYET
+//
+//  Created by YILMAZ ER on 5/7/19.
+//  Copyright © 2019 YILMAZ ER. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
